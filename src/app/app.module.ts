7fec7bfce0daad4bae4a9d406c4a272466bd5f14
Abhinand Manohar OP <103516348+abhinandop878@users.noreply.github.com>
2022-10-18 18:46:58 +0530

    admin login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    AdminLoginComponent
   ],
   imports: [
     BrowserModule,
